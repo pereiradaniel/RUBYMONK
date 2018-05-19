8e2e16ef04144a76e4c50c23c1dc7e4300e6b53a
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-19 09:30:20 -0400

    Change p to puts For consistency throughout files

--- RUBY_PRIMER/00_introduction_to_ruby_objects/00_introduction_to_objects.rb
+++ RUBY_PRIMER/00_introduction_to_ruby_objects/00_introduction_to_objects.rb
@@ -1,16 +1,16 @@
 #  In Ruby, everything is an object.
 
-p self
+puts self
 
 #  Talking to objects
 #  Interact with objects by calling methods on them.
 
-p 2.even?
+puts 2.even?
 
 #  Invoking a method will always generate a response, and the response is always another object.
 
-p 1.next()
+puts 1.next()
 
 #  Methods can also be chained.
 
-p 1.next().next()
+puts 1.next().next()
--- RUBY_PRIMER/00_introduction_to_ruby_objects/02_syntactic_sugar_for_special_methods.rb
+++ RUBY_PRIMER/00_introduction_to_ruby_objects/02_syntactic_sugar_for_special_methods.rb
@@ -2,16 +2,16 @@
 #  Methods can be called without a period to appear more logical
 
 #  All of these are correct:
-p 4.+(3)
-p 4.+ 3
-p 4 + 3
+puts 4.+(3)
+puts 4.+ 3
+puts 4 + 3
 
 #  Ruby makes an exception in its syntactic rules for commonly used operators so you don't have to use periods to invoke them on objects.
 
 #  The [] method does not require a period, and it encloses the arguments to itself:
 
-p words = ["foo", "bar", "baz"]
+puts words = ["foo", "bar", "baz"]
 
-p words[1]
-p words.[](1)
+puts words[1]
+puts words.[](1)
 
