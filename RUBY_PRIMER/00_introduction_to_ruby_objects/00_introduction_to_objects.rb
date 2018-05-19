5eaa9c1faa20b2f85bb5bd84699434a695b3233b
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-19 07:32:37 -0400

    Module completed Introduction To Ruby Objects Everything is an object, methods can be called on objeects and chained together

--- RUBY_PRIMER/00_introduction_to_ruby_objects/00_introduction_to_objects.rb
+++ RUBY_PRIMER/00_introduction_to_ruby_objects/00_introduction_to_objects.rb
@@ -1,3 +1,16 @@
 #  In Ruby, everything is an object.
 
-print self
+p self
+
+#  Talking to objects
+#  Interact with objects by calling methods on them.
+
+p 2.even?
+
+#  Invoking a method will always generate a response, and the response is always another object.
+
+p 1.next()
+
+#  Methods can also be chained.
+
+p 1.next().next()
