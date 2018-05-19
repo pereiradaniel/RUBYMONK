ee13a131945fd4c0ffd9e830a26f527e712cd2ad
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-19 08:23:10 -0400

    Invoking methods with arguments Call a method and pass one or more arguments Additional information is supplied to methods in the form of arguments

---
+++ RUBY_PRIMER/00_introduction_to_ruby_objects/01_more_objects_and_methods.rb
@@ -0,0 +1,12 @@
+#  Looking up methods
+
+p 1.methods
+
+#  Invoking methods with arguments
+#  When invoking a method, additional information can be passed using arguments.
+
+p ['rock','paper','scissors'].index('paper')
+
+#  More than one argument can be passed by separating them with commas.
+
+p 2.between?(1,3)
