a5694c3cc09c16c5347bb8891a8177fbc81f5a83
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-24 07:56:09 -0400

    00 boolean expressions in ruby Ruby uses the usual boolean operators

---
+++ RUBY_PRIMER/02_conditions_and_loops/00_boolean_expressions_in_ruby.rb
@@ -0,0 +1,19 @@
+#  Ruby uses the == operator for comparing to objects
+
+puts true == true
+
+#  The usual operators are also supported
+
+puts 10 < 100
+puts 50 >= 30
+
+#  Boolean expressions always return either 'true' or 'false'
+
+
+#  Combining Expressions using the && and || operators
+
+puts age >= && (name == 'Bob' || name == 'Jill')
+
+#  Negating expressions
+
+name != Bob
