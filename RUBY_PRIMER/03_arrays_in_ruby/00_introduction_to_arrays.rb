b152f7519ab4ec45c3a9a8afc17891bbfe26478d
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-29 11:34:16 -0400

    Introduction to arrays Arrays can be created using different methods There are several methods for retrieving data from arrays, and pushing into

--- RUBY_PRIMER/03_arrays_in_ruby/00_introduction_to_arrays.rb
+++ RUBY_PRIMER/03_arrays_in_ruby/00_introduction_to_arrays.rb
@@ -1,1 +1,23 @@
-00_introduction_to_arrays.rb
+#  Two ways to create a new array:
+[]
+Array.new
+
+puts [1,2,3,4,5]
+
+#  Arrays in Ruby allow you to store any kind of objects in any combination with no restrictions on type.
+
+#  Arrays have an index starting from 0
+puts [1,2,3][0]
+
+#  Array indexes can also start from the end of the array, rather tahn the beginning.  This is called reverse index lookup.
+puts [1,2,3,4,5][-5]  # To extract the first value
+puts [1,2,3,4,5][-1]  # To extract the last value
+
+
+#  Growing arrays
+#  The size of an array is not fixed.  Any type of object can be added to an array.
+array = [1,2,3,4,5]
+array << 6
+puts array
+array.push(7)
+puts array[-1]
