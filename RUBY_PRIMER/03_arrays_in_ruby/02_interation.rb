d407dc04f5e8c663abe11e1d7aa88cda4d86b4b0
Daniel Pereira <danielpaulpereira@gmail.com>
2018-05-29 11:56:38 -0400

    Arrays in ruby Iterate through arrays with different methods For loops and Array#each can do the same thing, for loops are less common.

---
+++ RUBY_PRIMER/03_arrays_in_ruby/02_interation.rb
@@ -0,0 +1,25 @@
+#  For loops
+array = [1,2,3,4,5]
+for i in array
+  puts i
+end
+
+
+def array_copy(source)
+  destination = []
+  for number in source
+    destination << number if number < 4
+  end
+  return destination
+end
+
+new_array = array_copy([1,2,3,4,5,6,7,8,9,10])
+p new_array
+
+#  Looping with 'each'
+
+def array_copy(source)
+  destination = []
+  source.each {|i| i < 4 ? destination << i: next}
+  return destination
+end
